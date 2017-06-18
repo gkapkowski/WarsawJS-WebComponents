01acd5c88d2f21614d0690d1b987586626682b97
Grzegorz Kapkowski <grzegorz.kapkowski@gmail.com>
2017-06-18 12:52:52 +0200

    #1 Mockup Element

---
+++ WebComponents/1-mockup/element/element.js
@@ -0,0 +1,21 @@
+class Element extends HTMLElement {
+    constructor () {
+        super();
+        console.log("constructor");
+
+        this.shadow = this.attachShadow({mode: "open"});
+    }
+
+    connectedCallback () {
+        let template = document.currentScript.ownerDocument.querySelector('template');
+
+        template = template.cloneNode(true);
+
+        template.content.querySelector('h1').innerText = this.attributes.label.value;
+        template.content.querySelector('img').src = this.attributes.photo.value;
+        
+        this.shadow.appendChild(template.content);
+    }
+}
+
+window.customElements.define('mockup-element', Element);
