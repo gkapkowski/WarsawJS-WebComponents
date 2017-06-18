54d95fabba7519247bcaadb9a2eb83486547fa94
Grzegorz Kapkowski <grzegorz.kapkowski@gmail.com>
2017-06-18 17:35:04 +0200

    #3 Media Player - init

---
+++ WebComponents/3-player/element/element.js
@@ -0,0 +1,50 @@
+class Element extends HTMLElement {
+    
+    constructor () {
+        super();
+        console.log("constructor");
+        this.shadow = this.attachShadow({mode: "open"});
+        this.template = document.currentScript.ownerDocument.querySelector('template');
+
+        this.index = 0;
+    }
+
+    connectedCallback () {
+        let template = this.template.cloneNode(true);
+        this.elem = template.content;
+        
+        this.elem.querySelector("button.play").addEventListener('click', this.play.bind(this));
+        this.elem.querySelector("button.stop").addEventListener('click', this.stop.bind(this));
+
+        this.shadow.appendChild(this.elem);
+
+        this.fill(this.index);
+    }
+
+    fill (index) {
+        let slide = this.shadow.querySelector("#slide");
+
+        while (slide.hasChildNodes()) {
+            slide.removeChild(slide.lastChild);
+        }
+
+        slide.appendChild(this.children[index].cloneNode(true));
+    }
+
+    play () {
+        this.timer = setInterval(() => {
+            console.log(this.index, this.children.length);
+            
+            this.fill(this.index);
+
+            this.index = this.index === this.children.length - 1 ? 0 : this.index + 1;
+        }, 2000);
+    }
+
+    stop () {
+        clearInterval(this.timer);
+    }
+
+}
+
+window.customElements.define('slides-player', Element);
